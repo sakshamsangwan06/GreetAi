7ff6e52a26af3952284e1f48c6284537ce3aa4f6
sakshamsangwan06 <156076142+sakshamsangwan06@users.noreply.github.com>
2025-08-22 02:52:35 +0530

    04 authentication-page

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -151,7 +151,7 @@ const onSubmit =(data: z.infer<typeof formSchema>) => {
                                 </Alert>
                             )}
                             <Button  disabled={pending} className="w-full" type="submit">
-                                Sign In 
+                                Sign Up 
                                 </Button>
                                 <div className="after:border-border relative text-center text-sm after:absolute
                                 after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
