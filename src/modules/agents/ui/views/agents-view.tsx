a82665904b9de0b095be81cdeaca317d91bb36ed
sakshamsangwan06 <156076142+sakshamsangwan06@users.noreply.github.com>
2025-08-31 16:00:25 +0530

    12 agents data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -6,6 +6,9 @@ import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 
 
@@ -14,8 +17,15 @@ export const AgentsView = () => {
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
     return (
-        <div>
-            {JSON.stringify(data, null, 2)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data ={data} columns={columns}/>
+            {data.length === 0 && (
+                <EmptyState
+                title="Create your first agent"
+                description="Get started by creating your first agent"
+                
+                />
+            )}
         </div>
     )
 }
