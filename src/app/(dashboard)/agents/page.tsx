f41924c926080b22dd971e63bdd59298917142fe
sakshamsangwan06 <156076142+sakshamsangwan06@users.noreply.github.com>
2025-08-31 13:59:34 +0530

    11 Agents Form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -8,13 +8,25 @@ import { AgentsView,
      AgentsViewError } 
      from "@/modules/agents/ui/views/agents-view";
 import { ErrorBoundary } from "react-error-boundary";
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 
 const Page = async() => {
+     const session = await auth.api.getSession({
+        headers: await headers(),
+      });
+      if(!session){
+        redirect("/sign-in");
+      }
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
     
     return (
+        <>
+        <AgentsListHeader/>
         <HydrationBoundary state={dehydrate(queryClient)}>
             <Suspense fallback={<AgentsViewLoading/>}>
             <ErrorBoundary fallback={<AgentsViewError/>} >
@@ -22,6 +34,7 @@ const Page = async() => {
         </ErrorBoundary>
         </Suspense>
         </HydrationBoundary>
+        </>
     );
 }
 export default Page;
