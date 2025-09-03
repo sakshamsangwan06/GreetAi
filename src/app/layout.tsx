3dfbb46c0d77867f44d1a4813160297d2ff6a407
sakshamsangwan06 <156076142+sakshamsangwan06@users.noreply.github.com>
2025-09-04 01:09:38 +0530

    13: agents filters

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NuqsAdapter} from "nuqs/adapters/next"
 
 import { TRPCReactProvider } from "@/trpc/client";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <NuqsAdapter>
     <TRPCReactProvider>
     <html lang="en">
       <body
@@ -34,5 +36,6 @@ export default function RootLayout({
       </body>
     </html>
     </TRPCReactProvider>
+    </NuqsAdapter>
   );
 }
